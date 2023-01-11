7acb5bfdd1b3612bf9c572959fd650516ebed8ce
sjtsh <74091619+sjtsh@users.noreply.github.com>
2023-01-12 01:41:37 +0545

    toggle

--- load.js
+++ load.js
@@ -1,17 +1,33 @@
-var popovers = document.querySelectorAll('.popover-button');
+var popovers = document.querySelectorAll('.popovers .solution-container');
 var popoverTriggers = document.querySelectorAll('.popover__trigger');
+var popoverWidget = document.querySelectorAll('.solution_popover');
 
 for (var i = 0; i < popoverTriggers.length; i++) {
-	popoverTriggers[i].addEventListener('click', function(event) {
-		closeAllOthers(this.parentElement);
-		this.parentElement.classList.toggle('popover--active');
-	});
+	popoverTriggers[i].addEventListener('click', async function (event) {
+		// if (!this.classList.contains('popover--active')) {
+		// 	this.classList.toggle('popover--active')
+		// }
+		// this.className += 'popover--active'
+		var cont = this.classList.contains('popover--active')
+		closeAllOthers()
+		if (!cont) {
+			this.classList.toggle('popover--active')
+		}
+		for (var i in popoverWidget) {
+			if (popoverWidget[i].classList.contains('popover--active')) {
+				popoverWidget[i].classList.remove('popover--active');
+				popoverWidget[i].className += " popover--inactive";
+			} else {
+				popoverWidget[i].classList.remove('popover--inactive');
+				popoverWidget[i].className += " popover--active";
+			}
+		}
+	}
+	)
 }
 
-function closeAllOthers(ignore) {
-	for (var i = 0; i < popovers.length; i++) {
-		if ( popovers[i] !== ignore) {
-			popovers[i].classList.remove('popover--active');	
-		}
+const closeAllOthers = () => {
+	for (var i = 0; i < popoverTriggers.length; i++) {
+		popoverTriggers[i].classList.remove('popover--active')
 	}
 }
